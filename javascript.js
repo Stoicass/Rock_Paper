2a2879773f70f2f1a9b91a78c2f8d98c30a0edd0
Stoicass <laeeq.boy369@gmail.com>
2024-07-14 10:31:00 +0500

    Making a funtion that returns a string value

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("hello World");
+function getComputerChoice()
+{
+    random = Math.random();
+    if (random >= 0 && random < 0.333)
+    {
+        choice = "rock"
+    }
+    else if (random >= 0.333 && random < 0.666 )
+    {
+        choice = "paper"
+    }
+    else
+    {
+        choice = "scissors"
+    }
+    return choice;
+}
+ 
+console.log(getComputerChoice());
